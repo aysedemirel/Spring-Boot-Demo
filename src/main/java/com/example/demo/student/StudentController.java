0a4b5d4e9acd2b4482a3e9e6de53bca9f6b325df
aysedemirel <aysedemirel1996@gmail.com>
2021-10-24 21:21:05 +0300

    [NEW] Add service layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,18 +1,24 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="api/v1/student")
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
-    public List<Student> hello() {
-        return List.of(new Student(1L, "Ayse", "aysedemireldeniz@gmail.com", LocalDate.of(1996, Month.JULY, 23), 25));
+    public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,2 +1,15 @@
-package com.example.demo.student;public class StudentService {
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "Ayse", "aysedemireldeniz@gmail.com", LocalDate.of(1996, Month.JULY, 23), 25));
+    }
 }
